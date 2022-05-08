9388b93cb47884149b05400593c942c2d209177f
DevFrog92 <yongsu1481@gmail.com>
2022-05-08 22:32:16 +0900

    🌱  setup router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
+
+// routes에서 lazy loading시 function arrow에서 {}로 컴포넌트를 감싸면 안된다.
+// wrapping되어서 반환되기 때문이다.
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import('../views/Home.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/test',
+    name: 'Test',
+    component: () => import('../views/Test.vue'),
   },
 ];
 
